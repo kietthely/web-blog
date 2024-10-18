2e83c72818860b0bb154d74c897ff81ce050f1c0
SammieDev <lythekiet333@gmail.com>
2024-10-18 21:24:57 +1030

    Establish some simple routes
    Added get and post routes which will be triggered from the home page.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  res.render("index.ejs", {});
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
