2455b87eb0e39a40dce18e417f9ca4236d764c27
SammieDev <lythekiet333@gmail.com>
2024-10-25 21:44:45 +1030

    Show the title of the post to the URL
    Additionally added a function to convert a string title to an ID to be searched by the local json file

--- app.js
+++ app.js
@@ -8,21 +8,26 @@ Step 1: Load data from the Json file named posts-storage
 
 Step 2: Route 3000/ will display a list of posts with the title and the first 100 characters of the content.
 
-Step 3: Route 3000/posts/:id will display the full content of the post with the corresponding id.
+ x Step 3: Route 3000/posts/:title will display the full content of the post with the corresponding id.
 
 Step 4: To create, users need to click on Create a Post and fill in the form.
 
-Step 5: The form will redirect user to their new post.
+  x Step 5: The form will redirect user to their new post.
 
 Step 6: The new post will be saved in the posts-storage file.
 
 Step 7: To edit it, users need to go to the post and click on Edit.
 
 */
+
+/*
+
+
+*/
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
-function readPostsDatabase(id) { 
+function readPostsDatabase(title) { 
   // load the next post id from the posts-storage file
   fs.readFile("posts-storage.json", "utf8", (err, data) => { 
     //TODO: error handler for reading the file
@@ -34,6 +39,10 @@ function readPostsDatabase(id) {
     console.log(posts);
 
   });
+}
+function convertrTitleToId(title) { 
+  return title.split(" ").join("-").toLowerCase();
+
 }
 
 function writePostsDatabase() {
@@ -50,7 +59,7 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.get("/posts/:id", (req, res) => {
+app.get("/posts/:title", (req, res) => {
   res.render("post.ejs", {
     id: req.params.id,
   });
